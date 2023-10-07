eeec2d03c5ac1d9518606e4a578aeee639e7f38e
Benhur A. Silva <benhur.sre@gmail.com>
2023-10-07 13:05:59 -0300

    feat(networking): create vpc and public subnets

---
+++ versions.tf
@@ -0,0 +1,26 @@
+terraform {
+  required_version = "~>1.6.0"
+
+  required_providers {
+    aws = {
+        source = "hashicorp/aws"
+        version = "~>5.19.0"
+    }
+  }
+  backend "s3" {
+    bucket = "platform-master"
+    key    = "tf-states/aws-environments/terraform.tfstate"
+    region = "eu-north-1"
+  }
+}
+
+provider "aws" {
+  region = "eu-north-1"
+  default_tags {
+    tags = {
+      project = var.project
+      owner= "Benhur A. Silva"
+      owner-linkedin = "linkedin.com/in/benhuraraujo/"
+    }
+  }
+}
