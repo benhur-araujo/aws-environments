e532772e5b53343fa2e9f083e6560e050ba41f0a
Benhur A. Silva <benhur.sre@gmail.com>
2023-10-12 15:08:26 -0300

    feat(oidc): add github identity provider

---
+++ oidc.tf
@@ -0,0 +1,6 @@
+# Add GitHub as an Identity Provider (IdP)
+resource "aws_iam_openid_connect_provider" "gh-oidc" {
+  url = "https://token.actions.githubusercontent.com"
+  client_id_list = ["sts.amazonaws.com"]
+  thumbprint_list = ["6938fd4d98bab03faadb97b34396831e3780aea1"]
+}
